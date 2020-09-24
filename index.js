b41aa96774f00c731d95f5a4b2b1d2b93517f002
Phantom <fay.ashbro@gmail.com>
2020-09-23 20:08:05 -0400

    prompt text created

--- index.js
+++ index.js
@@ -9,7 +9,50 @@ inquirer
         message: "What is the name of the project? ",
         name: "title"
 
+    },
+    {
+        type:"input",
+        message: "Description of this project",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What are the steps to install your project?",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Usage",
+        name: "usage"
+    },
+    {
+        type: "list",
+        choices:["Apache", "MIT", "GNU GPLv3", "none" ],
+        message: "What license would you like to use for this project?",
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "What are your guidelines for contributors? ",
+        name: "contributing"
+    },
+    {
+        type:"input", 
+        message: "Did you write tests for your application? If so provide examples.",
+        name: "tests"
+    },
+    {
+        type: "input",
+        message: "GitHub username",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your email?",
+        name: "email address"
     }
+
+
 ])
 .then(function (res){
     // console.log(response)
