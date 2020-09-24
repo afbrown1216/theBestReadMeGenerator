bea9486294ae66654c9a1e5875371095116efd1e
Phantom <fay.ashbro@gmail.com>
2020-09-24 17:16:39 -0400

    added license badge info

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,8 +1,16 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
     let licenseText = "";
+    let licenseBadge;
     if (data.license === "MIT"){
         licenseText = "This application is licensed under the MIT License . . .";
+        licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    } else if (data.license === "Apache"){
+        licenseText = ""
+        licenseBadge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+    }else if (data.license === "GNU GPLv3"){
+        licenseText;
+        licenseBadge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
     }
     return `
     
@@ -38,7 +46,7 @@ function generateMarkdown(data) {
     ${data.email}
 
     ## License 
-    
+
     ${data.license}
    
 
